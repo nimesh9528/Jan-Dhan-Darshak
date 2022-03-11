7e162c04bc091178341486f48c7cf6b945a37ede
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2022-03-11 23:14:28 +0530

    Splash Screen Added.

--- app/src/main/java/jan/dhan/darshak/MainActivity.kt
+++ app/src/main/java/jan/dhan/darshak/MainActivity.kt
@@ -2,10 +2,23 @@ package jan.dhan.darshak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import jan.dhan.darshak.databinding.ActivityMainBinding
+import jan.dhan.darshak.viewmodels.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/jan/dhan/darshak/viewmodels/MainActivityViewModel.kt
@@ -0,0 +1,20 @@
+package jan.dhan.darshak.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainActivityViewModel: ViewModel() {
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(1000)
+            _isLoading.value = false
+        }
+    }
+}
