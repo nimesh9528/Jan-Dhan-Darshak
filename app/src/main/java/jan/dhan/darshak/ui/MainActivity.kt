9f3fb805135bb9cd64e1c97438a7ea66be6da72d
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2022-03-12 22:55:45 +0530

    Bottom Navigation with Only 1 Fragment is Successful.

--- app/src/main/java/jan/dhan/darshak/ui/MainActivity.kt
+++ app/src/main/java/jan/dhan/darshak/ui/MainActivity.kt
@@ -1,16 +1,15 @@
 package jan.dhan.darshak.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
 import com.bhardwaj.navigation.SlideGravity
 import com.bhardwaj.navigation.SlidingRootNavBuilder
 import jan.dhan.darshak.R
 import jan.dhan.darshak.databinding.ActivityMainBinding
+import jan.dhan.darshak.fragments.MainFragment
 import jan.dhan.darshak.viewmodels.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -27,10 +26,40 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment?
-        val navController = navHostFragment!!.navController
-        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
+        initialise()
+        clickListeners()
+    }
+
+    private fun clickListeners() {
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.main -> {
+                    Toast.makeText(applicationContext, "Main", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.branch -> {
+                    Toast.makeText(applicationContext, "Branch", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.post_office -> {
+                    Toast.makeText(applicationContext, "Post Office", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.csc -> {
+                    Toast.makeText(applicationContext, "CSC", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.bank_mitra -> {
+                    Toast.makeText(applicationContext, "Bank Mitra", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
+    }
+
+    private fun initialise() {
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, MainFragment())
+            .commit()
 
         SlidingRootNavBuilder(this)
             .withMenuOpened(false)
